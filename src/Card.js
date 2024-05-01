4a2a0c5f43e92aab1147f61195ef5db8b3441912
Matheus <mateusmasi@gmail.com>
2024-05-01 14:31:26 -0300

    update

--- src/Card.js
+++ src/Card.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const Card = () => {
+const Card = (props) => {
 return (
-<div>
-<img alt='robots' src=' ' />
+<div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+<img alt='robots' src='https://www.robohash.org/test?200x200' />
     <div>
     <h2>Jane Doe</h2>
     <p>janedoe@gmail.com</p>
