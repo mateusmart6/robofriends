e1d46c7104873ffdd07f932d7efdab505476c217
Matheus <mateusmasi@gmail.com>
2024-05-01 16:18:57 -0300

    update

--- src/Card.js
+++ src/Card.js
@@ -1,16 +1,17 @@
 import React from 'react';
 
-const Card = (props) => {
-const {name, email, id } = props;  
-return (
-<div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5' id={id}>
-<img alt='robots' src={`https://www.robohash.org/${id}?200x200`} />
-    <div>
-    <h2>{name}</h2>
-    <p>{email}</p>
-    </div>
-</div>
-  );    
-}
+const Card = ({ name, email, id }) => {
+  return (
+    <>
+      <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5' id={id}>
+        <img alt='robots' src={`https://www.robohash.org/${id}?200x200`} />
+        <div>
+          <h2>{name}</h2>
+          <p>{email}</p>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Card; 
+export default Card;
